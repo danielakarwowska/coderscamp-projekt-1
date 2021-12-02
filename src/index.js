e73993b6d03944fe7470b677c88300adf43a6ed0
root <root@DESKTOP-TE6D32R.localdomain>
2021-12-02 23:16:17 +0100

    test

---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log('test1');
+console.log('test');
+
+function test(test) {
+  return test;
+}
+
+test(12);
+
+class Hej {
+  constructor(test) {
+    this.hej = test;
+  }
+}
+
+new Hej(test);
+
+console.log('another      shit');
