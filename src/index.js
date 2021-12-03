78ef30524beaaa51008617f923e57bde921063ab
WestMac <wester.maciej@gmail.com>
2021-12-03 02:44:55 +0100

    Added Husky nad Prettier for pre-commit format

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 console.log('test1');
-console.log('te             st');
-
-function test(test) {
-  return test;
-}
-
-test(12);
-
-class Hej {
-  constructor(test) {
-    this.hej = test;
-  }
-}
-
-console.log('elo');
-console.log('elo');
-console.log('heheheheh1');
