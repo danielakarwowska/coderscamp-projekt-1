a25497952a47d76a5787bd0e1d6b3e32f7787e27
WestMac <wester.maciej@gmail.com>
2021-12-07 15:40:06 +0100

    Refactored, added components folder and Theme component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
+import { Theme } from "./components/Theme.js";
 
-const toggle = document.getElementById("toggle"),
-sun = document.querySelector(".sun"),
-body = document.querySelector("body");
-let theme = "bright";
+toggle.addEventListener('click', Theme.themeChange);
 
-function setStyles(element, params) {
-for(let i in params) {
-element.style.setProperty(i, params[i]);
-}
-}
 
-function themeChange() {
-theme = theme === "bright" ? "dark" : "bright";
-document.querySelector("section > h2").textContent = `Too ${theme}?`;
 
-if(theme === "bright") {
-setStyles(body, {"--background": "#FBFFF1", "--text": "#3C3744", "--accent": "#3A6254"});
-
-toggle.classList.remove("clicked");
-sun.classList.remove("clicked");
-
-} else {
-setStyles(body, {"--background": "#3C3744", "--text": "#FBFFF1", "--accent": "#C41E3D"});
-
-toggle.classList.add("clicked");
-sun.classList.add("clicked");
-}
-}
-
-toggle.addEventListener("click", themeChange);
