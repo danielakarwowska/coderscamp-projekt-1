ed6b4863ee87885c982c0b8994c496b264b826e5
Listek93 <radek@janecki.info>
2021-12-14 20:46:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,22 @@ back.addEventListener('mouseover', on)
 front.addEventListener('mouseleave', off)
 back.addEventListener('mouseleave', off)
 
+function register () {
+  const login = document.getElementById('login');
+  const mail = document.getElementById('mail');
+  const password = document.getElementById('password');
+  localStorage.setItem ("login", login.value );
+  localStorage.setItem ("password", password.value);
+  localStorage.setItem ("mail", mail.value);
+};
+
+
+// function login () {
+//   const loginToApp = document.getElementById('login-in-login');
+//   const passwordToApp = document.getElementById('password-in-login');
+// }
+
+
 
 
 
