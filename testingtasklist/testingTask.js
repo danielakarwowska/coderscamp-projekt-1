b1dcbd2cd0880b9bcf384f681f455d4bc0ab4dbc
ImTheRealXixe <kuba.jarocki1@gmail.com>
2021-12-17 20:19:17 +0100

    sorting added

--- testingtasklist/testingTask.js
+++ testingtasklist/testingTask.js
@@ -147,3 +147,43 @@ document.getElementById('task-list').addEventListener('click', (e) => {
 klasa showAlert tworzy nowego diva z klasą 'alert', który trzeba wysylizować
 tak samo klasa showAlertSuccess => div z klasą 'success'.
 */
+
+// SORTING
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll('tr'));
+
+    // sort row
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs 
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Add again the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how it's currently sorted
+
+    table.querySelectorAll('th').forEach(th => th.classList.remove('th-sort-asc', 'th-sort-desc'));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-asc', asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-desc', !asc);
+}
+
+document.querySelectorAll('.table-sortable th').forEach(headerCell => {
+    headerCell.addEventListener('click', () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAsc = headerCell.classList.contains('th-sort-asc');
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAsc);
+    });
+});
